43e6ace277b64fc5baca853f8d3801d86b9d2581
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-30 11:08:19 +0530

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -1,10 +1,6 @@
-const {cmd , commands} = require('../command')
-
 cmd({
-    pattern: "apk",
-    desc: "apk the bot",
-    category: "main",
-    react: "✅",
+    pattern: "dapk",
+    dontAddCommandList: true,
     filename: __filename
 },
 async (conn, m, mek, { from, q, reply }) => {
@@ -12,7 +8,7 @@ try{
 await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
 if(!q) return await conn.sendMessage(from , { text: '*Need apk link...*' }, { quoted: mek } ) 
 const data = await apkdl.download(q)
-let sendapk = await conn.sendMessage(from , { document : { url : data.dllink  } , mimetype : 'application/vnd.android.package-archive' , fileName : data.name + '.' + 'apk',caption: '*ꜱʜᴀᴅᴏᴡ ᴍᴅ ʙᴏᴛ ᴡᴀᴛꜱ ᴀᴘᴘ*' } , { quoted: mek })
+let sendapk = await conn.sendMessage(from , { document : { url : data.dllink  } , mimetype : 'application/vnd.android.package-archive' , fileName : data.name + '.' + 'apk',caption: '*ᴠᴀᴊɪʀᴀ-ᴍᴅ•ᴍᴜʟᴛɪ-ᴅᴇᴠɪᴄᴇ*\n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴛᴇᴄʜɴɪᴄᴀʟ ᴄʏʙᴇʀꜱ*' } , { quoted: mek })
 await conn.sendMessage(from, { react: { text: '📁', key: sendapk.key }})
 await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
 } catch (e) {
