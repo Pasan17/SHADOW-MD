233eeb8af4d3ac7b40d643bf9789d0763dd9b354
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-30 10:25:45 +0530

    Create apk.js

---
+++ plugins/apk.js
@@ -0,0 +1,18 @@
+cmd({
+    pattern: "dapk",
+    dontAddCommandList: true,
+    filename: __filename
+},
+async (conn, m, mek, { from, q, reply }) => {
+try{
+await conn.sendMessage(from, { react: { text: '📥', key: mek.key }})
+if(!q) return await conn.sendMessage(from , { text: '*Need apk link...*' }, { quoted: mek } ) 
+const data = await apkdl.download(q)
+let sendapk = await conn.sendMessage(from , { document : { url : data.dllink  } , mimetype : 'application/vnd.android.package-archive' , fileName : data.name + '.' + 'apk',caption: '*ꜱʜᴀᴅᴏᴡ ᴍᴅ ʙᴏᴛ ᴡᴀᴛꜱ ᴀᴘᴘ*' } , { quoted: mek })
+await conn.sendMessage(from, { react: { text: '📁', key: sendapk.key }})
+await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
+} catch (e) {
+    reply('*ERROR !!*')
+  l(e)
+}
+})
