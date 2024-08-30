b3b79903776d3c7fcf455ab0fc626ec2190717e8
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-30 23:47:25 +0530

    Update grup.js

--- plugins/grup.js
+++ plugins/grup.js
@@ -1,1 +1,105 @@
-h
+const config = require('../config')
+const os = require('os')
+const fs = require('fs')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson, jsonformat} = require('../lib/functions')
+const { default: makeWASocket, useMultiFileAuthState, WA_DEFAULT_EPHEMERAL, jidNormalizedUser, proto, getDevice, generateWAMessageFromContent, fetchLatestBaileysVersion, makeInMemoryStore, getContentType, generateForwardMessageContent, downloadContentFromMessage, jidDecode } = require('@whiskeysockets/baileys')
+
+
+cmd({
+    pattern: "mute",
+    react: "✅",
+    desc: "close a group",
+    category: "group",
+    use: '.mute',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+if (!isGroup) return reply(ONLGROUP)
+if (!isBotAdmins) return reply(botAdmin)
+if (!isAdmins) return reply(ADMIN)
+                                  
+        await conn.groupSettingUpdate(mek.chat, 'announcement')
+        const sendmsg = await conn.sendMessage(mek.chat.G_MUTE)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('🛑 GROUP IS CLOSED MY BOT OWNER')
+l(e)
+}
+})
+
+
+  
+cmd({
+    pattern: "unmute",
+    react: "✅",
+    desc: "open a group",
+    category: "group",
+    use: '.unmute',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+if (!isGroup) return reply(ONLGROUP)
+if (!isBotAdmins) return reply(botAdmin)
+if (!isAdmins) return reply(ADMIN)
+                                  
+        await conn.groupSettingUpdate(mek.chat, 'not_announcement')
+        const sendmsg = await conn.sendMessage(mek.chat.G_UNMUTE)
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('🛑 GROUP IS OPEN MY BOT OWNER')
+l(e)
+}
+})
+
+
+cmd({
+    pattern: "promote",
+    react: "✅",
+    desc: "promote admin to a member",
+    category: "group",
+    use: '.promote',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+if (!isGroup) return reply(ONLGROUP)
+if (!isBotAdmins) return reply(botAdmin)
+if (!isAdmins) return reply(ADMIN)
+                                  
+         let users = mek.mentionedJid ? mek.mentionedJid : mek.quoted ? mek.quoted.sender : q.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+		await conn.groupParticipantsUpdate(mek.chat, [users], 'promote').then((res) => reply(jsonformat(res))).catch((err) => reply(jsonformat(err)))
+	reply('🛑 GROUP ADMIN PROMOTE BY MY BOT OWNER')
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Done ✓✓*')
+l(e)
+}
+}) 
+
+
+cmd({
+    pattern: "demote",
+    react: "✅",
+    desc: "demote admin to a member",
+    category: "group",
+    use: '.demote',
+    filename: __filename
+},
+async(conn, mek, m,{from, prefix, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{                   
+if (!isGroup) return reply(ONLGROUP)
+if (!isBotAdmins) return reply(botAdmin)
+if (!isAdmins) return reply(ADMIN)
+                                  
+         	let users = mek.mentionedJid ? mek.mentionedJid : mek.quoted ? mek.quoted.sender : q.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+		await conn.groupParticipantsUpdate(mek.chat, [users], 'demote').then((res) => reply(jsonformat(res))).catch((err) => reply(jsonformat(err)))
+reply('🛑 GROUP ADMIN DEMOTE BY MY BOT OWNER')
+await conn.sendMessage(from, { react: { text: `✅`, key: mek.key }}) 
+} catch (e) {
+reply('*Done ✓✓*')
+l(e)
+}
+})
