dcb360786c1e0eb182e7631519c76bcf4c121873
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-08-31 09:23:16 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,6 +10,5 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68
 SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: "true",
-MONGOOB: process.env.MONGOOB || "enter mongodb here",
+AUTO_VOICE: "true"
 };
