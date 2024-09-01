0983946c5239d4f0232054a0a587e8bfc758a4c0
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-09-01 16:03:13 +0530

    Update g+pramo.js

--- plugins/g+pramo.js
+++ plugins/g+pramo.js
@@ -1,9 +1,8 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
-
 cmd({
-    pattern: "promote",
-    react: "📍",
+    pattern: "demote",
+    react: "⛔",
     desc: "Provides admin role to replied/quoted user",
     category: "group",
     filename: __filename,
@@ -12,13 +11,13 @@ cmd({
       async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 if(!isOwner ||  !isAdmins )return;
          try {     if (!m.isGroup) return reply(mg.onlygroup);
-    if (!isBotAdmins) return reply(mg.needbotadmins);
+            if (!isBotAdmins) return reply(mg.needbotadmins);
 
         let users = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? citel.quoted.sender : q.replace(/[^0-9]/g, "") + "@s.whatsapp.net";
-        if (!users) return reply(mg.nouserforpromote);
-        await conn.groupParticipantsUpdate(m.chat, [users], "promote");
+        if (!users) return reply(mg.nouserfordemote);
+        await conn.groupParticipantsUpdate(m.chat, [users], "demote");
 } catch (e) {
-reply('✅')
+reply('*Error !!*')
 l(e)
 }
 })
