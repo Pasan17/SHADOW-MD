b05df898a948c1db8b86905207e01a07056e4626
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-09-02 14:21:17 +0530

    Update apk.js

--- plugins/apk.js
+++ plugins/apk.js
@@ -5,7 +5,7 @@ cmd({
     alias: ["modapk"],
     desc: "download apks",
     category: "download",
-    react: "👾",
+    react: "⚡",
     filename: __filename
 },
 async(conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
