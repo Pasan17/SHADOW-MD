ab7ca3b1d604e2b0712bdcd575394bd44ce0fe09
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-09-02 09:29:30 +0530

    Create grup+wel.js

---
+++ plugins/grup+wel.js
@@ -0,0 +1,25 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+cmd({
+    pattern: "setwelcome",
+    desc: "Set the welcome message for the group.",
+    category: "group",
+    react: "👋",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply('This command can only be used in a group.')
+        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
+        if (!isAdmins) return reply('You must be an admin to use this command.')
+
+        const welcome = q
+        if (!welcome) return reply('Please provide a welcome message.')
+
+        await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: welcome })
+        await reply('Welcome message has been set.')
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
