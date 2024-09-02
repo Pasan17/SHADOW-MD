cd8a865ec7f0e932912566ea49311a37eba25a37
Pasan17 <pasansanjana047@gmail.com>
2024-09-02 15:33:05 +0000

    Pending changes exported from your codespace

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SESSION_ID: process.env.SESSION_ID || "RmkUDJyY#lWwsJETcKQeop0fl5nr907mGmAfCswmTm2Erlez3Egc",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/357b75726739a8b9c68de.jpg",
 SUDO_NB: process.env.SUDO_NB || "ඔයාගෙ owner number එක දෙන්න",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
