dea46964a9d315aed3e16cc54e75a245117e4d01
QUEEN KYLIE - MD🔰 <mdshadow692@gmail.com>
2024-09-02 09:31:02 +0530

    Create grup+setby.js

---
+++ plugins/grup+setby.js
@@ -0,0 +1,25 @@
+const config = require('../config')
+const { cmd, commands } = require('../command')
+cmd({
+    pattern: "setgoodbye",
+    desc: "Set the goodbye message for the group.",
+    category: "group",
+    react: "😪",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        if (!isGroup) return reply('This command can only be used in a group.')
+        if (!isBotAdmins) return reply('Bot must be an admin to use this command.')
+        if (!isAdmins) return reply('You must be an admin to use this command.')
+
+        const goodbye = q
+        if (!goodbye) return reply('Please provide a goodbye message.')
+
+        await conn.sendMessage(from, { image: { url: config.ALIVE_IMG }, caption: goodbye })
+        await reply('Goodbye message has been set.')
+    } catch (e) {
+        console.log(e)
+        reply(`${e}`)
+    }
+})
